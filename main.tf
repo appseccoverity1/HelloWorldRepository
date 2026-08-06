7b5f0995bc1ece50d296ca0a4d6b093bb1de3409
AppDevSecops <58944360+appseccoverity1@users.noreply.github.com>
2026-08-06 14:19:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-resource "aws_instance" "ramapp_server" {
+resource "aws_instance" "ram_server" {
   ami           = "ami-02b64aa047cb5edf5"
   instance_type = "t3.micro"
 
