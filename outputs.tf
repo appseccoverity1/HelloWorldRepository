89b05de7a0f2a40acb410cdc80d6554c488bba45
AppDevSecops <58944360+appseccoverity1@users.noreply.github.com>
2026-08-06 16:48:37 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "testing"
 output "instance_id" {
-description="ID of the EC2 instance"
-value = aws_instance.ram_server.id
+  description = "ID of the EC2 instance"
+  value       = aws_instance.ram_server.id
 }
-output "instance_public_ip"{
-description="Public IP address of the EC2 instance"
-value = aws_instance.ram_server.public_ip
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.ram_server.public_ip
 }
