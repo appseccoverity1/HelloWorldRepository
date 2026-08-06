f4626b0bb94b85bf1c1fd65e3b35465095241dff
AppDevSecops <58944360+appseccoverity1@users.noreply.github.com>
2026-08-06 16:46:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,20 @@
+variable "ami_id" {
+  default = "ami-02b64aa047cb5edf5"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "instance_name" {
+  default = "learn-terraform"
+}
+
 resource "aws_instance" "ram_server" {
-  ami           = "ami-02b64aa047cb5edf5"
-  instance_type = "t3.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
 
   tags = {
-    Name = "var.learn-terraform"
+    Name = var.instance_name
   }
 }
