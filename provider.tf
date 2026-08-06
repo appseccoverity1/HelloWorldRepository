3df54bd12b110b1021fb82fb2c3fc37909a93baa
AppDevSecops <58944360+appseccoverity1@users.noreply.github.com>
2026-08-06 14:28:13 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+backend "s3"{
+  bucket= "terraform-infosectrain-remote-backend"
+key= "terraform.tfstate"
+region= "us-west-2"
+}
+ required_version=">=1.5.0"
+}
+
+}
+
 provider "aws" {
   region = "us-east-1"
 }
